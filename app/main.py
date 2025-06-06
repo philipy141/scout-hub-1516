8d821548c76383f02fb90b409116938cb3e4cc4e
philipy141 <philipylferreira@gmail.com>
2025-06-06 14:33:24 +0200

    Fix Streamlit Cloud import issue

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from app.services.data_loader import load_players_df
+from services.data_loader import load_players_df
 
 st.set_page_config(page_title="Scout Hub 15/16", layout="wide")
 st.title("Scout Hub 2015/16 – MVP")
